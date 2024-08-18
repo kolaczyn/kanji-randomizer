690f58c4602dc39c9a41056f1f46b442cc77ee18
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2024-08-18 14:39:38 +0200

    Remove seed from url

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,17 +1,14 @@
 import { Link } from "react-router-dom";
 import { Button, Container } from "@chakra-ui/react";
-import { useRandomNum } from "../hooks/useRandomNum.ts";
 
 export const StartScreen = () => {
-  const rand = useRandomNum();
-  const urlWithRand = (path: string) => `${path}?seed=${rand}`;
   return (
     <Container>
       <h1>Kanji Test</h1>
-      <Link to={urlWithRand("/n5")}>
+      <Link to="/n5">
         <Button>JLPT N5 kanji</Button>
       </Link>
-      <Link to={urlWithRand("/n4")}>
+      <Link to="/n4">
         <Button>JLPT N4 kanji</Button>
       </Link>
     </Container>
--- src/hooks/useRandomNum.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from "react";
-
-const decimalToBigNum = (num: number): number =>
-  Number(num.toString().replace("0.", ""));
-
-export const useRandomNum = () =>
-  useMemo(() => decimalToBigNum(Math.random()), []);
