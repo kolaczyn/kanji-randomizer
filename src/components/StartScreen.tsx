8b2902c53faf7e35717cd465a24571fa4b9213bc
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2024-08-18 18:53:25 +0200

    add N3, N2, N1 kanji

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,15 +1,29 @@
 import { Link } from "react-router-dom";
 import { Button, Container } from "@chakra-ui/react";
+import { kanjiN5 } from "../const/kanjiN5.ts";
+import { kanjiN4 } from "../const/kanjiN4.ts";
+import { kanjiN3 } from "../const/kanjiN3.ts";
+import { kanjiN2 } from "../const/kanjiN2.ts";
+import { kanjiN1 } from "../const/kanjiN1.ts";
 
 export const StartScreen = () => {
   return (
     <Container>
       <h1>Kanji Test</h1>
       <Link to="/n5">
-        <Button>JLPT N5 kanji</Button>
+        <Button>N5 kanji ({kanjiN5.length})</Button>
       </Link>
       <Link to="/n4">
-        <Button>JLPT N4 kanji</Button>
+        <Button>N4 kanji ({kanjiN4.length})</Button>
+      </Link>
+      <Link to="/n3">
+        <Button>N3 kanji ({kanjiN3.length})</Button>
+      </Link>
+      <Link to="/n2">
+        <Button>N2 kanji ({kanjiN2.length})</Button>
+      </Link>
+      <Link to="/n1">
+        <Button>N1 kanji ({kanjiN1.length})</Button>
       </Link>
     </Container>
   );
--- src/utils/getDeck.ts
+++ src/utils/getDeck.ts
@@ -1,9 +1,24 @@
 import { KanjiList, Level } from "../types.ts";
-import { kanjiN4, kanjiN5 } from "../const/kanji.ts";
+import { kanjiN5 } from "../const/kanjiN5.ts";
+import { kanjiN4 } from "../const/kanjiN4.ts";
+import { kanjiN1 } from "../const/kanjiN1.ts";
+import { kanjiN3 } from "../const/kanjiN3.ts";
+import { kanjiN2 } from "../const/kanjiN2.ts";
 
 export const getDeck = (level: Level): KanjiList | null => {
   const lvl = level.toLowerCase();
-  if (lvl === "n5") return kanjiN5;
-  if (lvl === "n4") return kanjiN4;
-  return null;
+  switch (lvl) {
+    case "n5":
+      return kanjiN5;
+    case "n4":
+      return kanjiN4;
+    case "n3":
+      return kanjiN3;
+    case "n2":
+      return kanjiN2;
+    case "n1":
+      return kanjiN1;
+    default:
+      return null;
+  }
 };
