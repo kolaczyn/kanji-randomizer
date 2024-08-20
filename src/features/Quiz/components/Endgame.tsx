f2c3f696127f0efe3604dcad8f4bfcf1718278d2
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2024-08-20 10:42:13 +0200

    fix showing incorrect twice

--- src/features/Quiz/components/Endgame.tsx
+++ src/features/Quiz/components/Endgame.tsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 import { Button } from "@chakra-ui/react";
 import { useAtom } from "jotai/react";
 import { deckAtom } from "../../../state/deckAtom.ts";
+import { useSettings } from "../../../hooks/useSettings.ts";
 
 export const Endgame = () => {
   const [{ incorrect }] = useAtom(deckAtom);
+  const [{ showIncorrect }] = useSettings();
+  const shouldShowIncorrect = incorrect.length > 0 && !showIncorrect;
   return (
     <>
       <h2>No more cards</h2>
-      {incorrect.length > 0 ? <IncorrectKanji /> : null}
+      {shouldShowIncorrect ? <IncorrectKanji /> : null}
       <Link to="/">
         <Button>Exit</Button>
       </Link>
