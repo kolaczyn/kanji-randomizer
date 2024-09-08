a425ffd5652859eed1494b857cb65b146df3a12e
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2024-09-08 11:11:10 +0200

    remove all remaining references to old api

--- src/hooks/useFetchDeck.ts
+++ src/hooks/useFetchDeck.ts
@@ -1,14 +1,13 @@
-import { KanjiList, Level } from "../types.ts";
+import { KanjiList } from "../types.ts";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { API_BASE_URL } from "../const/env.ts";
 
-export const useFetchDeck = (lvl: Level) => {
-  const type = lvl.startsWith("n") ? "kanji" : "kana";
+export const useFetchDeck = (id: string) => {
   return useQuery({
-    queryKey: ["kanji", lvl],
+    queryKey: ["deck", id],
     queryFn: async () => {
-      const res = await axios.get<KanjiList>(`${API_BASE_URL}/${type}/${lvl}`);
+      const res = await axios.get<KanjiList>(`${API_BASE_URL}/v2/decks/${id}`);
       return res.data;
     },
     staleTime: Infinity,
